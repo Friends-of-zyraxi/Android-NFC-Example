fcf950b75e7c68cf46fd7392744172ff2f78c83c
zyraxi <zyraxi@outlook.com>
2026-09-11 15:20:29 +0800

    chore(release): 版本号更新为 20260911
    同步更新 app/build.gradle.kts 的 versionName 与界面显示的版本标签。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 34
         targetSdk = 37
         versionCode = 1
-        versionName = "20260910"
+        versionName = "20260911"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
